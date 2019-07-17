a7a3b63b4c65efc26d72255cb0ceca2589349028
Neung0 <Neung0@users.noreply.github.com>
2019-07-17 17:30:54 +0900

    view&html test

--- search/views.py
+++ search/views.py
@@ -1,5 +1,12 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import View
+# from .forms import BookForm
+from search.models import Author
+
+# class BookFormView(View):
+#     form_class = BookForm
 
 def index(request):
-    return HttpResponse('Hello, World. You are at the search index.')
+    auth_list = Author.objects.all().order_by('name')
+    context = {'auth_list': auth_list}
+    return render(request, 'search/index.html', context)
